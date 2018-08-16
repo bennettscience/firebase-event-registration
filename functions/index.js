d453cfb9645d595a8ff56598a41c3b288fabd0cb
Brian E Bennett <brian@ohheybrian.com>
2018-08-15 21:37:21 -0400

    User email notifications
    Set up with cron-job.org, run a search every day for classes two days ahead. If
    one is found, send an email to all registered users, reminding them of their
    signup.
    
    Still needs location information added, but may just point users to the PD site
    instead of building a more complex function at this point.

--- functions/index.js
+++ functions/index.js
@@ -15,85 +15,95 @@ const mailTransport = nodemailer.createTransport({
 
 const ref = admin.database().ref();
 
-exports.countRegistrations = functions.database.ref('/courses/{courseId}/members/{uid}').onWrite(
-  (change) => {
-      const collectionRef = change.after.ref.parent;
-      const countRef = collectionRef.parent.child('seats');
-
-      let increment;
-      if (change.after.exists() && !change.before.exists()) {
-        increment = -1;
-      } else if (!change.after.exists() && change.before.exists()) {
-        increment = 1;
-      } else {
-        return null;
-      }
-
-      // Return the promise from countRef.transaction() so our function
-      // waits for this async event to complete before it exits.
-      return countRef.transaction((current) => {
-        return (current || 0) + increment;
-      }).then(() => {
-        return console.log('Counter updated.');
-      });
-    });
-
-// If the number of likes gets deleted, recount the number of likes
-exports.recountlikes = functions.database.ref('/courses/{courseId}/members/{uid}').onDelete((snap) => {
-  const counterRef = snap.ref;
-  const collectionRef = counterRef.parent.child('seats');
-
-  // Return the promise from counterRef.set() so our function
-  // waits for this async event to complete before it exits.
-  return collectionRef.once('value')
-      .then((messagesData) => counterRef.set(messagesData.numChildren()));
-});
+// exports.countRegistrations = functions.database.ref('/courses/{courseId}/members/{uid}').onWrite(
+//   (change) => {
+//       const collectionRef = change.after.ref.parent;
+//       const countRef = collectionRef.parent.child('seats');
+//
+//       let increment;
+//       if (change.after.exists() && !change.before.exists()) {
+//         increment = -1;
+//       } else if (!change.after.exists() && change.before.exists()) {
+//         increment = 1;
+//       } else {
+//         return null;
+//       }
+//
+//       // Return the promise from countRef.transaction() so our function
+//       // waits for this async event to complete before it exits.
+//       return countRef.transaction((current) => {
+//         return (current || 0) + increment;
+//       }).then(() => {
+//         return console.log('Counter updated.');
+//       });
+//     });
+//
+// // If the number of likes gets deleted, recount the number of likes
+// exports.recountlikes = functions.database.ref('/courses/{courseId}/members/{uid}').onDelete((snap) => {
+//   const counterRef = snap.ref;
+//   const collectionRef = counterRef.parent.child('seats');
+//
+//   // Return the promise from counterRef.set() so our function
+//   // waits for this async event to complete before it exits.
+//   return collectionRef.once('value')
+//       .then((messagesData) => counterRef.set(messagesData.numChildren()));
+// });
 
 exports.reminderEmail = functions.https.onRequest((req, res) => {
   const currentTime = new Date().getTime();
   const future = currentTime + 172800000;
+  const mailOpts = {};
 
   return ref.child('courses/').orderByChild('start').startAt(future).once('value')
   .then(snap => {
     const promises = [];
-
     snap.forEach(child => {
       var el = child.val();
 
       if(el.hasOwnProperty('members')) {
-        promises.push(admin.database().ref('courses/' + child.key + '/members/').once('value'))
+        promises.push(admin.database().ref('courses/' + child.key).once('value'))
       }
     })
-
     return Promise.all(promises)
   })
   .then(results => {
-    console.log(results)
     const emails = [];
     results.forEach(delta => {
-      console.log(delta.val());
-      delta.forEach(member => {
-        var email = member.val().email;
-        emails.push(email);
-      })
+      var course = delta.val();
+      mailOpts.subject = "Upcoming registration for " + course.title;
+      if(course.members) {
+        return admin.database().ref('courses/' + delta.key + '/members').once('value')
+        .then(data => {
+          data.forEach(member => {
+            var email = member.val().email;
+            emails.push(email)
+          })
+          return Promise.all(emails)
+        })
+        .then(emails => {
+          // console.log(emailList)
+        //   const emails = [];
+        //   members.forEach(member => {
+        //     var email = member.email;
+        //     emails.push(email);
+        //   });
+
+          // console.log('Line 79: Sending to: ' + emails.join());
+          mailOpts.from = '"Elkhart PD" <pd@elkhart.k12.in.us>',
+          mailOpts.bcc = emails.join(),
+          mailOpts.text = 'Something about registering here.'
+
+          console.log(mailOpts)
+          return mailTransport.sendMail(mailOpts)
+        })
+        .then(() => {
+          res.send('Email sent')
+        })
+        .catch(error => {
+          res.send(error)
+        })
+      }
     })
-
-    console.log('Line 79: Sending to: ' + emails.join());
-
-    const mailOpts = {
-      from: '"Elkhart PD" <pd@elkhart.k12.in.us>',
-      bcc: emails.join(),
-      subject: 'Upcoming Registration',
-      text: 'Something about registering here.'
-    }
-
-    console.log(mailOpts)
-    return mailTransport.sendMail(mailOpts)
   })
-  .then(() => {
-    res.send('Email sent')
-  })
-  .catch(error => {
-    res.send(error)
-  })
+
 })
