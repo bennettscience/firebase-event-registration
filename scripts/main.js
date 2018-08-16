7cf1f838a6f4a2c0a75bcf7690b635335b600429
Brian E Bennett <brian@ohheybrian.com>
2018-08-16 10:02:20 -0400

    User subscriptions feature
    Users can successfully register and listen for notifications with a service
    worker. The specific `device_id` is stored in the `users/` node and is
    completely removed on unsubscribe.

--- scripts/main.js
+++ scripts/main.js
@@ -454,6 +454,126 @@ PDReg.prototype.cancel = function(e) {
   this.database.ref('courses/' + id).on('value', buildCourse)
 }
 
+const messaging = firebase.messaging(),
+      database  = firebase.database(),
+      pushBtn   = document.getElementById('push-button')
+
+let userToken    = null,
+    isSubscribed = false
+
+window.addEventListener('load', () => {
+
+    if ('serviceWorker' in navigator) {
+
+        navigator.serviceWorker.register('scripts/service-worker.js')
+            .then(registration => {
+
+                messaging.useServiceWorker(registration)
+
+                initializePush()
+            })
+            .catch(err => console.log('Service Worker Error', err))
+
+    } else {
+        pushBtn.textContent = 'Push not supported.'
+    }
+
+})
+
+function initializePush() {
+
+    userToken = localStorage.getItem('pushToken')
+
+    isSubscribed = userToken !== null
+    updateBtn()
+
+    pushBtn.addEventListener('click', () => {
+        pushBtn.disabled = true
+
+        if (isSubscribed) return unsubscribeUser()
+
+        return subscribeUser()
+    })
+}
+
+function updateBtn() {
+
+    if (Notification.permission === 'denied') {
+        pushBtn.textContent = 'Subscription blocked'
+        return
+    }
+
+    pushBtn.textContent = isSubscribed ? 'Unsubscribe' : 'Subscribe'
+    pushBtn.disabled = false
+}
+
+function subscribeUser() {
+
+    messaging.requestPermission()
+        .then(() => messaging.getToken())
+        .then(token => {
+
+            updateSubscriptionOnServer(token)
+            isSubscribed = true
+            userToken = token
+            localStorage.setItem('pushToken', token)
+            updateBtn()
+        })
+        .catch(err => console.log('Denied', err))
+
+}
+
+function updateSubscriptionOnServer(token) {
+  console.log(isSubscribed, token)
+    if (isSubscribed) {
+        return database.ref('users/' + firebase.auth().currentUser.uid + '/device_ids')
+                .equalTo(token)
+                .on('child_added', snapshot => snapshot.ref.remove())
+    }
+
+    database.ref('users/' + firebase.auth().currentUser.uid + '/device_ids').once('value')
+        .then(snapshots => {
+            let deviceExists = false
+
+            snapshots.forEach(childSnapshot => {
+                if (childSnapshot.val() === token) {
+                    deviceExists = true
+                    return console.log('Device already registered.');
+                }
+
+            })
+
+            if (!deviceExists) {
+                M.toast({html: 'Successfully subscribed to reminders'})
+                return database.ref('users/' + firebase.auth().currentUser.uid + '/device_ids').push(token)
+            }
+        })
+}
+
+function unsubscribeUser() {
+
+    messaging.deleteToken(userToken)
+        .then(() => {
+            //updateSubscriptionOnServer(userToken)
+            return database.ref('users/' + firebase.auth().currentUser.uid + '/device_ids').once('value')
+            .then(snap => {
+              snap.forEach(token => {
+                if(token.val() === userToken) {
+                  snap.ref.remove();
+                }
+              })
+            })
+            .then(() => {
+                isSubscribed = false
+                userToken = null
+                localStorage.removeItem('pushToken')
+                updateBtn()
+                M.toast({ html: 'Successfully unsubscribed from notifications.' })
+              })
+            })
+        .catch(err => console.log('Error unsubscribing', err))
+}
+
 
 /**
  *  Load the PDReg Class and initialize Firebase.
