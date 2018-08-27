ff6fc17f79385ceb61b300cf7089a3c4099853e2
Brian E Bennett <brian@ohheybrian.com>
2018-08-27 10:59:23 -0400

    debugged seat counter function

--- functions/index.js
+++ functions/index.js
@@ -15,31 +15,31 @@ const mailTransport = nodemailer.createTransport({
 
 const ref = admin.database().ref();
 
-// exports.countRegistrations = functions.database.ref('/courses/{courseId}/members/{uid}').onWrite(
-//   (change) => {
-//       const collectionRef = change.after.ref.parent;
-//       const countRef = collectionRef.parent.child('seats');
-//
-//       let increment;
-//       if (change.after.exists() && !change.before.exists()) {
-//         increment = -1;
-//       } else if (!change.after.exists() && change.before.exists()) {
-//         increment = 1;
-//       } else {
-//         return null;
-//       }
-//
-//       // Return the promise from countRef.transaction() so our function
-//       // waits for this async event to complete before it exits.
-//       return countRef.transaction((current) => {
-//         return (current || 0) + increment;
-//       }).then(() => {
-//         return console.log('Counter updated.');
-//       });
-//     });
-//
-// // If the number of likes gets deleted, recount the number of likes
-// exports.recountlikes = functions.database.ref('/courses/{courseId}/members/{uid}').onDelete((snap) => {
+exports.countRegistrations = functions.database.ref('/courses/{courseId}/members/{uid}').onWrite(
+  (change) => {
+      const collectionRef = change.after.ref.parent;
+      const countRef = collectionRef.parent.child('seats');
+
+      let increment;
+      if (change.after.exists() && !change.before.exists()) {
+        increment = -1;
+      } else if (!change.after.exists() && change.before.exists()) {
+        increment = 1;
+      } else {
+        return null;
+      }
+
+      // Return the promise from countRef.transaction() so our function
+      // waits for this async event to complete before it exits.
+      return countRef.transaction((current) => {
+        return (current || 0) + increment;
+      }).then(() => {
+        return console.log('Counter updated.');
+      });
+    });
+
+// If the number of likes gets deleted, recount the number of likes
+// exports.recountlikes = functions.database.ref('/courses/{courseId}').onDelete((snap) => {
 //   const counterRef = snap.ref;
 //   const collectionRef = counterRef.parent.child('seats');
 //
