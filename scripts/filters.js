7fba7b141f1decf09542305c94209320348a6e18
bennettscience <brian.bennett2@gmail.com>
2019-06-04 10:54:02 -0400

    fix end time formatting bug

--- scripts/filters.js
+++ scripts/filters.js
@@ -1,46 +1,70 @@
-
 /**
  * Load sideNav script from materialize.js
  */
 
 $(document).ready(function() {
-  $(".sidenav").sidenav({
+  $('.sidenav').sidenav({
     draggable: true,
     edge: 'left',
     preventScrolling: true,
-    isFixed: true
+    isFixed: true,
   });
 
-  var el = $("#user-building-select")
-  var schools = ["Beardsley", "Beck", "Bristol", "Cleveland", "Daly", "Eastwood", "Feeser", "Hawthorne", "Monger", "Osolo", "Pinewood", "Riverview", "Roosevelt", "Woodland", "North Side", "Pierre Moran", "West Side", "Elkhart Academy",
-      "Central", "Memorial", "EACC", "Community Education", "PACE", "Administration", "Tech Services", "Building Services", "Transportation", "School Without Walls"
+  var el = $('#user-building-select');
+  var schools = [
+    'Beardsley',
+    'Beck',
+    'Bristol',
+    'Cleveland',
+    'Daly',
+    'Eastwood',
+    'Feeser',
+    'Hawthorne',
+    'Monger',
+    'Osolo',
+    'Pinewood',
+    'Riverview',
+    'Roosevelt',
+    'Woodland',
+    'North Side',
+    'Pierre Moran',
+    'West Side',
+    'Elkhart Academy',
+    'Central',
+    'Memorial',
+    'EACC',
+    'Community Education',
+    'PACE',
+    'Administration',
+    'Tech Services',
+    'Building Services',
+    'Transportation',
+    'School Without Walls',
   ];
   var sorted = schools.sort();
 
   for (var i = 0; i < sorted.length; i++) {
-    var opt = document.createElement('option')
+    var opt = document.createElement('option');
     opt.text = sorted[i];
     opt.value = sorted[i];
-    opt = '<option value="' + sorted[i] + '">' + sorted[i] + '</option>'
-    el.append(opt)
+    opt = '<option value="' + sorted[i] + '">' + sorted[i] + '</option>';
+    el.append(opt);
   }
 
   $('#user-building-select').formSelect();
 
   $('.collapsible').collapsible();
-
-})
+});
 
 $(document).keydown(function(e) {
-  if(e.keyCode == 27) {
-      if($("#user-courses-list").hasClass('hidden')){
+  if (e.keyCode == 27) {
+    if ($('#user-courses-list').hasClass('hidden')) {
       return false;
     } else {
-      $("#user-courses-wrap").addClass('hidden');
+      $('#user-courses-wrap').addClass('hidden');
     }
   }
 });
-
 
 /**
  * getBg - Get a random header background
@@ -56,7 +80,6 @@ function getBg() {
   return base + rand + ext;
 }
 
-
 /**
  * Filter by title
  *
@@ -64,25 +87,31 @@ function getBg() {
  *
  * Searches for sessions by title
  */
-$(".input-field input").on('keyup',function() {
+$('.input-field input').on('keyup', function() {
   /**
    * @event Hide
    * @param {String} String input by the user.
    */
   // var dataLayer = [];
-  $(".class-container").hide();
-  var input = $(this).val().toUpperCase();
-  $(".card-title").each(function() {
-    var title = $(this).text().toUpperCase();
-    if(title.indexOf(input) > -1) {
-      $(this).parents(".class-container").show();
+  $('.class-container').hide();
+  var input = $(this)
+    .val()
+    .toUpperCase();
+  $('.card-title').each(function() {
+    var title = $(this)
+      .text()
+      .toUpperCase();
+    if (title.indexOf(input) > -1) {
+      $(this)
+        .parents('.class-container')
+        .show();
     }
   });
 
   this.onblur = function() {
     console.log(this.value);
-    window.dataLayer.push({'event': 'search', 'searchTerm': this.value})
-  }
+    window.dataLayer.push({ event: 'search', searchTerm: this.value });
+  };
 });
 
 // Filter by Danielson category
@@ -91,36 +120,35 @@ $("input[name='filterStatus']").change(function() {
 
   $("input[name='filterStatus']").each(function() {
     if ($(this).is(':checked')) {
-      classes.push($(this).val())
+      classes.push($(this).val());
     }
-  })
+  });
 
-  if (classes == "") {
-    $("#allCourses .card").show();
+  if (classes == '') {
+    $('#allCourses .card').show();
   } else {
-    $("#allCourses .card").hide()
+    $('#allCourses .card').hide();
 
-    $("#allCourses .card").each(function() {
+    $('#allCourses .card').each(function() {
       var filter = [];
       var show = false;
       var card = $(this);
 
-      filter = card.data('dan').split(",");
+      filter = card.data('dan').split(',');
 
       var who = [];
 
-      for(var n=0; n<classes.length;n++) {
-        if(filter.includes(classes[n])){
+      for (var n = 0; n < classes.length; n++) {
+        if (filter.includes(classes[n])) {
           show = true;
         }
       }
       if (show) {
-        card.show()
+        card.show();
       }
-    })
+    });
   }
-})
-
+});
 
 /**
  * Clear the search form
@@ -129,64 +157,83 @@ $("input[name='filterStatus']").change(function() {
  *
  * Clears the search input
  */
-$("#search-clear").on('click', function() {
+$('#search-clear').on('click', function() {
   /**
    *  @param {Object} parent DOM object
    */
-  $(this).parent().find('input').val('');
-  $("#search-form").toggleClass('hidden').blur()
-  $(".class-container").show();
-})
+  $(this)
+    .parent()
+    .find('input')
+    .val('');
+  $('#search-form')
+    .toggleClass('hidden')
+    .blur();
+  $('.class-container').show();
+});
 
-$("#search-button").on('click', function() {
-  $(this).next().toggleClass('hidden')
-  $("#search").focus()
-})
+$('#search-button').on('click', function() {
+  $(this)
+    .next()
+    .toggleClass('hidden');
+  $('#search').focus();
+});
 
-$("#slide-out-button").on('click', function() {
-  console.log('clicked')
+$('#slide-out-button').on('click', function() {
+  console.log('clicked');
   $('#slide-out').sidenav('open');
-})
+});
 
-$("#course-toggle").on('change', function() {
-  console.log('clicked toggle')
-  $("div.class-container").toggleClass('hidden');
-})
+$('#course-toggle').on('change', function() {
+  console.log('clicked toggle');
+  $('div.class-container').toggleClass('hidden');
+});
 
 // Format dates for sessions displayed in main container
 function format(date) {
   var d = new Date(date);
-  var m = moment(d).format('dddd, MMM D, YYYY - h:mm A');
+  var m = moment(d).format('dddd, MMM D, YYYY, h:mm A');
   return m;
 }
 
 // Format dates for user list
 function smallFormat(date) {
   var d = new Date(date);
-  var m = moment(d).format('MM/D/YY - h:00 A');
+  var m = moment(d).format('MM/D/YY - h:mm A');
   return m;
 }
 
+function formatEnd(date) {
+  var d = new Date(date);
+  var m = moment(d).format('h:mm A');
+  return m;
+}
 
 // Frontend validation for session code
 // Turns code field green if the key is correct, case sensitive
-$("body").on("keyup",".code :input", function() {
-  var row = $(this).closest('.card').prop('id');
+$('body').on('keyup', '.code :input', function() {
+  var row = $(this)
+    .closest('.card')
+    .prop('id');
   var key = $(this).val();
 
-  for(var key in codes) {
-    if(codes[key].id === row) {
-
+  for (var key in codes) {
+    if (codes[key].id === row) {
       // validates the length of the user-input code. If >0, require the checkbox.
-      if($(this).val().length > 0) {
-        $(this).closest('.card-content').find('input:checkbox').prop('required', true);
+      if ($(this).val().length > 0) {
+        $(this)
+          .closest('.card-content')
+          .find('input:checkbox')
+          .prop('required', true);
       } else {
-        $(this).closest('.card-content').find('input:checkbox').prop('required', false);
+        $(this)
+          .closest('.card-content')
+          .find('input:checkbox')
+          .prop('required', false);
       }
 
       // Validate the code
-      if(codes[key].code === $(this).val()) {
-        $(this).css('background-color','rgba(0,255,0,0.4)');
+      if (codes[key].code === $(this).val()) {
+        $(this).css('background-color', 'rgba(0,255,0,0.4)');
       } else {
         $(this).css('background-color', 'white');
       }
@@ -195,26 +242,39 @@ $("body").on("keyup",".code :input", function() {
 });
 
 // Sort options - date, alphabetical
-$("#title").on('click', function() {
-  var divs = $("div.class-container");
+$('#title').on('click', function() {
+  var divs = $('div.class-container');
 
   var alphabeticalDivs = divs.sort(function(a, b) {
-    return String.prototype.localeCompare.call($(a).data('title').toLowerCase(), $(b).data('title').toLowerCase());
-  })
+    return String.prototype.localeCompare.call(
+      $(a)
+        .data('title')
+        .toLowerCase(),
+      $(b)
+        .data('title')
+        .toLowerCase(),
+    );
+  });
 
-  var container = $("#allCourses");
-  container.detach().empty().append(alphabeticalDivs)
-  $("#course-form").append(container)
-})
+  var container = $('#allCourses');
+  container
+    .detach()
+    .empty()
+    .append(alphabeticalDivs);
+  $('#course-form').append(container);
+});
 
-$("#date").on('click', function() {
-  var divs = $("div.class-container");
+$('#date').on('click', function() {
+  var divs = $('div.class-container');
 
-  var dateDivs = divs.sort(function(a,b) {
+  var dateDivs = divs.sort(function(a, b) {
     return new Date($(a).data('date')) - new Date($(b).data('date'));
   });
 
-  var container = $("#allCourses")
-  container.detach().empty().append(dateDivs)
-  $("#course-form").append(container);
-})
+  var container = $('#allCourses');
+  container
+    .detach()
+    .empty()
+    .append(dateDivs);
+  $('#course-form').append(container);
+});
