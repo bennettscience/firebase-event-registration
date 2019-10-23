3e420932418e98a7fa32008aec970aa9f86c8bf6
bennettscience <brian.bennett2@gmail.com>
2019-10-23 11:28:59 -0400

    Merge bugfix/email-reminders

--- functions/index.js
+++ functions/index.js
@@ -25,7 +25,6 @@ exports.onlineRegConfirmation = functions.database
 		let mailOpts = {};
 		const user = snap.val();
 		mailOpts.to = user.email;
-		// console.log('The user is ', user);
 
 		const course = snap.ref.parent.parent;
 
@@ -40,11 +39,10 @@ exports.onlineRegConfirmation = functions.database
 						el.title
 					}. This is an online course, so please visit the <a href='${
 						el.redirect
-					}' target='_blank'>Canvas login page</a> to begin.</p><p>If you have trouble, please <a href='mailto:${
+					}' target='_blank'>course page</a> to begin.</p><p>If you have trouble, please <a href='mailto:${
 						el.pocEmail
 					}'>contact the course facilitator</a> for more help.</p><p>---</p><p>Elkhart Professional Development</p>`;
 
-					// console.log(mailOpts);
 					return mailTransport.sendMail(mailOpts);
 				} else {
 					return null;
@@ -81,18 +79,6 @@ exports.countRegistrations = functions.database
 			});
 	});
 
-// If the number of likes gets deleted, recount the number of likes
-// exports.recountlikes = functions.database.ref('/courses/{courseId}').onDelete((snap) => {
-//   const counterRef = snap.ref;
-//   const collectionRef = counterRef.parent.child('seats');
-//
-//   // Return the promise from counterRef.set() so our function
-//   // waits for this async event to complete before it exits.
-//   return collectionRef.once('value')
-//       .then((messagesData) => counterRef.set(messagesData.numChildren()));
-// });
-
-// TODO: Return promise if no emails are sent
 exports.twoDayReminder = functions.https.onRequest(async (req, res) => {
 	let today = Date.now();
 	let future = today + (36 * 60 * 60 * 1000);
@@ -122,6 +108,8 @@ exports.twoDayReminder = functions.https.onRequest(async (req, res) => {
 			members.forEach(user => {
 				users.push(user.val().email);
 			});
+
+			users.push(item.submittedBy, item.pocEmail)
 			
 			mailOpts.subject = `Upcoming registration for ${item.title}`;
 			mailOpts.to = 'pd@elkhart.k12.in.us';
@@ -177,6 +165,8 @@ exports.oneWeekReminder = functions.https.onRequest(async (req, res) => {
 				users.push(user.val().email);
 			});
 
+			users.push(item.submittedBy, item.pocEmail)
+
 			mailOpts.subject = `Upcoming registration for ${item.title}`;
 			mailOpts.to = 'pd@elkhart.k12.in.us';
 			mailOpts.bcc = users.join(',');
@@ -195,135 +185,8 @@ exports.oneWeekReminder = functions.https.onRequest(async (req, res) => {
 			// console.log(users.join(','));
 			mailTransport.sendMail(mailOpts);
 		});
-		res.send('Email sent');
+		res.send(`Email sent`);
 	} catch (err) {
 		res.send(err);
 	}
-
-
-
-	// return ref.child('courses/').orderByChild('start').once('value')
-	// 	.then(snap => {
-	// 		let promises = [];
-	// 		snap.forEach(child => {
-	// 			// TODO: Only send emails to events exactly seven days in the future
-	// 			var el = child.val();
-	// 			let startTimestamp = new Date(el.start).getTime();
-	// 			if (startTimestamp > future && startTimestamp <= ffuture) {
-	// 				if(el.hasOwnProperty('members')) {
-	// 					promises.push(admin.database().ref('courses/' + child.key).once('value'));
-	// 				} else {
-	// 					console.log('There are no registrations to send!');
-	// 				}
-	// 			}
-	// 		});
-	// 		return Promise.all(promises);
-	// 	})
-	// 	.then(results => {
-	// 		let emails = [];
-	// 		results.forEach(delta => {
-	// 			var course = delta.val();
-	// 			console.log(course.title);
-	// 			responses.push(course.title);
-	// 			courseOpts.title = course.title;
-	// 			courseOpts.loc = course.loc;
-	// 			courseOpts.date = new Date(course.start).toLocaleDateString();
-	// 			courseOpts.time = new Date(course.start).toLocaleTimeString();
-	// 			mailOpts.subject = 'Upcoming registration for ' + course.title;
-	// 			if (course.members) {
-	// 				return admin.database().ref('courses/' + delta.key + '/members').once('value')
-	// 					.then(data => {
-	// 						data.forEach(member => {
-	// 							var email = member.val().email;
-	// 							emails.push(email);
-	// 						});
-	// 						return Promise.all(emails);
-	// 					})
-	// 					.then(emails => {
-	// 						//console.log('Line 79: Sending to: ' + emails.join());
-	// 						mailOpts.from = '"Elkhart PD" <pd@elkhart.k12.in.us>',
-	// 						mailOpts.bcc = 'bbennett@elkhart.k12.in.us',
-	// 						// mailOpts.bcc = emails.join(),
-	// 						mailOpts.html = `
-	// 							<p>This is a reminder that you're currently scheduled to attend <b>${courseOpts.title}</b> on <b>${courseOpts.date}</b>.</p>
-	// 							<ul>
-	// 								<li>Workshop: ${courseOpts.title}</li>
-	// 								<li>Location: ${courseOpts.loc}</li>
-	// 								<li>Start time: ${courseOpts.time}</li>
-	// 							</ul>
-	// 							<p>Please visit the <b><a href="https//pd.elkhart.k12.in.us">Elkhart PD website</a></b> for more details or to cancel your registration if you can no longer attend.</p>
-	// 							<br />
-	// 							<b>Elkhart Professional Development</b>
-	// 						`;
-
-	// 						// return JSON.stringify(mailOpts);
-	// 						return mailTransport.sendMail(mailOpts);
-	// 					})
-	// 					.then((mailOpts) => {
-	// 						res.send(`Emails sent to ${responses}`);
-	// 					})
-	// 					.catch(error => {
-	// 						res.send(error);
-	// 					});
-	// 			}
-	// 		});
-	// 	});
 });
-
-// exports.sendPostNotification = functions.database.ref('/courses/{courseId}}').onCreate(snapshot => {
-// 	var course = snapshot.val();
-// 	const title = course.title;
-// 	const rawDate = new Date(course.start);
-// 	var date = rawDate.getDate() + '/' + rawDate.getMonth() + '/' + rawDate.getYear();
-
-// 	// // if data deleted => exit
-// 	// if (!postTitle) return console.log('Post', postID, 'deleted')
-// 	//
-// 	// // Get Device tokens
-// 	const getDeviceTokensPromise = admin
-// 		.database()
-// 		.ref('device_ids')
-// 		.once('value')
-// 		.then(snapshots => {
-// 			//
-// 			//     // Check if tokens exist
-// 			if (!snapshots) {
-// 				return console.log('No device IDs to send notifications to.');
-// 			}
-// 			//
-// 			//     // Notification details
-// 			const payload = {
-// 				notification: {
-// 					body: `${title} is available on ${date}. Click to sign up.`,
-// 				},
-// 			};
-// 			snapshots.forEach(childSnapshot => {
-// 				const token = childSnapshot.val();
-
-// 				// Send notification to all tokens
-// 				admin
-// 					.messaging()
-// 					.sendToDevice(token, payload)
-// 					.then(response => {
-// 						response.results.forEach(result => {
-// 							const error = result.error;
-
-// 							if (error) {
-// 								console.error('Failed delivery to', token, error);
-
-// 								// Prepare unused tokens for removal
-// 								if (
-// 									error.code === 'messaging/invalid-registration-token' ||
-// 									error.code === 'messaging/registration-token-not-registered'
-// 								) {
-// 									childSnapshot.ref.remove();
-// 									console.info('Was removed:', token);
-// 								}
-// 							} else {
-// 								console.info('Notification sent to', token);
-// 							}
-// 						});
-// 					});
-// 			});
-// 		});
-// });
